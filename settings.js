59dea4e6af9e29744c9c5a31d363e06592cbf864
Danysikazwe1000 <165155739+Danysikazwe1000@users.noreply.github.com>
2025-05-20 22:53:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Barbie',
+  author: '‎Dc',
+  botName: "Barbie Bot",
+  botOwner: 'Barbie', // Your name
+  ownerNumber: '255785612486', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
